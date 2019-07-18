ff9198019d7772c89834cd7a1fde6742d7fb1142
ullahasad386 <ullahasad386@gmail.com>
2019-07-18 12:56:34 +0500

    Configured rails helpher file and added some gems

---
+++ spec/rails_helper.rb
@@ -0,0 +1,22 @@
+require 'database_cleaner'
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
+RSpec.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
