051c83222f0d9afa4dd7904320d4c59ce4e66783
ullahasad386 <ullahasad386@gmail.com>
2019-07-19 11:37:15 +0500

    Added Category model and implemented validation tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_075841) do
+ActiveRecord::Schema.define(version: 2019_07_19_062158) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name"
